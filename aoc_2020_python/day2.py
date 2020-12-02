abe463ca7aee2931f42d7145db581192873d87b0
Fungu <christian.lundell@gmail.com>
2020-12-02 08:07:34 +0100

    2020-02 use scanf for input parsing

--- aoc_2020_python/day2.py
+++ aoc_2020_python/day2.py
@@ -1,16 +1,14 @@
 import time
+import scanf
 
 def main():
     with open("input/day2.txt") as file:
         lines = file.readlines()
     part1 = 0
     part2 = 0
-    
+
     for line in lines:
-        left, password = line.split(":")
-        password = password.strip()
-        low, right = left.split("-")
-        high, letter = right.split(" ")
+        low, high, letter, password = scanf.scanf("%d-%d %c: %s", line)
         count = password.count(letter)
         if count >= int(low) and count <= int(high):
             part1 += 1
