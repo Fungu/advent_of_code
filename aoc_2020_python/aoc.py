8c21b01a4e9da14a1afaa0830237c901ea9c3fa3
Fungu <christian.lundell@gmail.com>
2020-12-07 12:23:03 +0100

    Refactor day7

---
+++ aoc_2020_python/aoc.py
@@ -0,0 +1,18 @@
+import time
+
+def runLines(mainFunction, day):
+    with open("input/" + day) as file:
+        rawInput = file.readlines()
+    run(mainFunction, rawInput)
+
+def runBlob(mainFunction, day):
+    with open("input/" + day) as file:
+        rawInput = file.read()
+    run(mainFunction, rawInput)
+
+def run(mainFunction, rawInput):
+    start = time.time()
+    part1, part2 = mainFunction(rawInput)
+    print("Execution time:", time.time() - start, "ms")
+    print("Part 1:", part1)
+    print("Part 2:", part2)
