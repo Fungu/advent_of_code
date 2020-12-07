16e754b49408988a57af726f6ad632c3849e88e0
Fungu <christian.lundell@gmail.com>
2020-12-07 07:57:14 +0100

    refactor day6

--- aoc_2020_python/day6.py
+++ aoc_2020_python/day6.py
@@ -1,13 +1,15 @@
 import time
 import functools
 
-def main():
-    with open("input/day6.txt") as file:
-        rawInput = file.read()
-    
-    print("Part 1:", sum([len(group) for group in [{allAnswers for allAnswers in group.replace("\n", "")} for group in rawInput.split("\n\n")]]))
-    print("Part 2:", sum([len(group) for group in [functools.reduce(lambda a, b: list(set(a) & set(b)), group) for group in [[groupAnswers for groupAnswers in group.split("\n")] for group in rawInput.split("\n\n")]]]))
+def main(rawInput):
+    part1 = sum([len(group) for group in [{allAnswers for allAnswers in group.replace("\n", "")} for group in rawInput.split("\n\n")]])
+    part2 = sum([len(group) for group in [functools.reduce(lambda a, b: list(set(a) & set(b)), group) for group in [[groupAnswers for groupAnswers in group.split("\n")] for group in rawInput.split("\n\n")]]])
+    return part1, part2
 
+with open("input/day6.txt") as file:
+    rawInput = file.read()
 start = time.time()
-main()
+part1, part2 = main(rawInput)
 print("Execution time:", time.time() - start, "ms")
+print("Part 1:", part1)
+print("Part 2:", part2)
