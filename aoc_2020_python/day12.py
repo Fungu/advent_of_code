2cd1f226c0139e49bdc295cfa676c9ba4a39d183
Fungu <christian.lundell@gmail.com>
2020-12-12 07:34:30 +0100

    Day12 comments

--- aoc_2020_python/day12.py
+++ aoc_2020_python/day12.py
@@ -1,33 +1,37 @@
 import aoc
 
 def main(inputLines):
-    part1 = followInstructions(inputLines, 0 + 0j, 1 + 0j, True)
-    part2 = followInstructions(inputLines, 0 + 0j, 10 + 1j, False)
+    part1 = followInstructions(inputLines, 1 + 0j, True)
+    part2 = followInstructions(inputLines, 10 + 1j, False)
     
     return part1, part2
 
-def followInstructions(instructions, position, direction, part1):
-    dirDict = { "E": 1, "W": -1, "N": 1j, "S": -1j}
-    rotDict = { "L": 1j, "R": -1j}
+def followInstructions(instructions, speed, part1):
+    # Complex numbers are  used for position and rotation. The real part represents East and West. The imaginary part represents North and South.
+    # To rotate a direction, just multiply it with the corresponding value in rotations.
+    # Example: directions["E"] * rotations["L"] == directions["N"]
+    directions = { "E": 1, "W": -1, "N": 1j, "S": -1j }
+    rotations = { "L": 1j, "R": -1j }
+    position = 0 + 0j
 
     for line in instructions:
         action = line[0]
         value = int(line[1:])
         # E, W, N, S
-        if action in dirDict:
+        if action in directions:
             if part1:
-                position += dirDict[action] * value
+                position += directions[action] * value
             else:
-                direction += dirDict[action] * value
+                speed += directions[action] * value
         # L, R
-        elif action in rotDict:
-            assert(value % 90 == 0)
-            direction *= rotDict[action] ** int(value / 90)
+        elif action in rotations:
+            assert value % 90 == 0
+            speed *= rotations[action] ** int(value / 90)
         # F
         elif action == "F":
-            position += direction * value
+            position += speed * value
         else:
-            assert(False)
+            assert False
     
     return int(abs(position.real) + abs(position.imag))
 
