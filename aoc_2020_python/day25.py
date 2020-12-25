b606372f62fc37618b9c62ce329662153bf635fa
Christian Lundell <christian.lundell@gmail.com>
2020-12-25 08:56:42 +0100

    day25 improvements

--- aoc_2020_python/day25.py
+++ aoc_2020_python/day25.py
@@ -4,28 +4,16 @@ def main(inputLines):
     doorPublicKey = int(inputLines[0])
     cardPublicKey = int(inputLines[1])
 
-    doorLoopSize = findLoopSize(doorPublicKey)
-    doorEncryptionKey = transform(doorLoopSize, cardPublicKey)
-
-    return doorEncryptionKey, "Pay 49 stars"
-
-def findLoopSize(publicKey):
-    loopSize = 0
-    subjectNumber = 7
-    value = 1
+    doorKeyCandidate = 1
+    encryptionKey = 1
     while True:
-        loopSize += 1
-        value *= subjectNumber
-        value = value % 20201227
-        if value == publicKey:
-            return loopSize
+        encryptionKey *= cardPublicKey
+        encryptionKey = encryptionKey % 20201227
+        doorKeyCandidate *= 7
+        doorKeyCandidate = doorKeyCandidate % 20201227
+        if doorKeyCandidate == doorPublicKey:
+            break
 
-
-def transform(loopSize, subjectNumber):
-    value = 1
-    for _ in range(loopSize):
-        value *= subjectNumber
-        value = value % 20201227
-    return value
+    return encryptionKey, "Pay 49 stars"
 
 aoc.runLines(main, "day25.txt")
