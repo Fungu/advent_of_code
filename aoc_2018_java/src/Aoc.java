4e992c632e66cddb05efab06bd509952f10889ae
Fungu <christian.lundell@gmail.com>
2021-01-06 14:27:19 +0100

    Aoc time measurement

--- aoc_2018_java/src/Aoc.java
+++ aoc_2018_java/src/Aoc.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.time.LocalTime;
 
 public class Aoc {
     public static void runLines(AocSolver solver) {
@@ -11,10 +10,10 @@ public class Aoc {
     
     public static void runLines(AocSolver solver, String inputFileString) {
         try {
-            LocalTime start = LocalTime.now();
+        	long start = System.currentTimeMillis();
             List<String> inputLines = Files.readAllLines(Path.of("input/" + inputFileString));
             solver.solve(inputLines);
-            System.out.println("Execution time: " + LocalTime.now().compareTo(start) + " ms");
+            System.out.println("Execution time: " + (System.currentTimeMillis() - start) + " ms");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -26,18 +25,18 @@ public class Aoc {
     
     public static void runRaw(AocSolver solver, String inputFileString) {
         try {
-            LocalTime start = LocalTime.now();
+            long start = System.currentTimeMillis();
             String inputLines = Files.readString(Path.of("input/" + inputFileString));
             solver.solve(inputLines);
-            System.out.println("Execution time: " + LocalTime.now().compareTo(start) + " ms");
+            System.out.println("Execution time: " + (System.currentTimeMillis() - start) + " ms");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     
-    private static LocalTime prevTime = LocalTime.now();
+    private static long prevTime = System.currentTimeMillis();
     public static void timeDiff(String message) {
-    	System.out.println(message + " " + LocalTime.now().compareTo(prevTime));
-    	prevTime = LocalTime.now();
+    	System.out.println(message + " " + (System.currentTimeMillis() - prevTime));
+    	prevTime = System.currentTimeMillis();
     }
 }
