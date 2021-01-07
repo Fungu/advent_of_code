0e18758f462f1047f69dd86173c137a11091865e
Fungu <christian.lundell@gmail.com>
2021-01-07 21:52:52 +0100

    Fix warnings

--- aoc_2018_java/src/Day08.java
+++ aoc_2018_java/src/Day08.java
@@ -1,10 +1,5 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Day08 implements AocSolver {
 	public static void main(String[] args) {
