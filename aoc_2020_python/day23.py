3efdaf891efcb2c983b0f07b529902c63feca344
Fungu <christian.lundell@gmail.com>
2021-01-09 11:05:27 +0100

    day23 performance

--- aoc_2020_python/day23.py
+++ aoc_2020_python/day23.py
@@ -4,34 +4,6 @@ class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
-       self.prev = None
-
-class CircularLinkedList:
-    def __init__(self):
-        self.first = None
- 
-    def insert_after(self, ref_node, new_node):
-        new_node.prev = ref_node
-        new_node.next = ref_node.next
-        new_node.next.prev = new_node
-        ref_node.next = new_node
- 
-    def insert_at_end(self, new_node):
-        if self.first is None:
-            self.first = new_node
-            new_node.next = new_node
-            new_node.prev = new_node
-        else:
-            self.insert_after(self.first.prev, new_node)
- 
-    def remove(self, node):
-        if self.first.next == self.first:
-            self.first = None
-        else:
-            node.prev.next = node.next
-            node.next.prev = node.prev
-            if self.first == node:
-                self.first = node.next
  
 def main(inputBlob):
     part1, _ = play(inputBlob, 0, 100)
@@ -41,17 +13,24 @@ def main(inputBlob):
 
 def play(inputBlob, extraCups, iterations):
     nrOfCups = max(len(inputBlob), extraCups)
-    cups = CircularLinkedList()
+    firstNode = None
+    lastNode = None
     cupDict = {}
     for a in inputBlob:
         node = Node(int(a))
-        cups.insert_at_end(node)
+        if firstNode == None:
+            firstNode = node
+        if lastNode != None:
+            lastNode.next = node
         cupDict[int(a)] = node
+        lastNode = node
     for a in range(max([int(b) for b in inputBlob]) + 1, extraCups + 1):
         node = Node(a)
-        cups.insert_at_end(node)
         cupDict[a] = node
-    currentCup = cupDict[int(inputBlob[0])]
+        lastNode.next = node
+        lastNode = node
+    currentCup = firstNode
+    lastNode.next = firstNode
 
     for _ in range(iterations):
         pickup = []
@@ -59,8 +38,7 @@ def play(inputBlob, extraCups, iterations):
         for _ in range(3):
             pickup.append(c)
             c = c.next
-        pickup[0].prev.next = pickup[2].next
-        pickup[2].next.prev = pickup[0].prev
+        currentCup.next = pickup[-1].next
         
         destination = currentCup.data - 1
         if destination < 1:
@@ -78,10 +56,8 @@ def play(inputBlob, extraCups, iterations):
                 break
         
         destinationCup = cupDict[destination]
-        destinationCup.next.prev = pickup[2]
         pickup[2].next = destinationCup.next
         destinationCup.next = pickup[0]
-        pickup[0].prev = destinationCup
 
         currentCup = currentCup.next
     
