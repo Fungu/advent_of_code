5e274b3a5e64810ff9dfe44c6dcd5adb3e85f9cd
Fungu <christian.lundell@gmail.com>
2021-12-01 20:55:39 +0100

    2021 day01

---
+++ aoc_2021_rust/day01/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, self};
+
+fn main() -> io::Result<()> {
+    let buf_reader = BufReader::new(File::open("input.txt")?);
+    let values = buf_reader
+        .lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let part1 = count_increases(values.clone());
+
+    let mut sliding_values: Vec<i32> = Vec::new();
+    for i in 0 .. values.len() - 2 {
+        sliding_values.push(values[i] + values[i+1] + values[i+2]);
+    }
+
+    let part2 = count_increases(sliding_values.clone());
+
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
+
+    Ok(())
+}
+
+fn count_increases(values: Vec<i32>) -> i32 {
+    let mut ret = 0;
+    for i in 0 .. values.len() - 1 {
+        if values[i + 1] > values[i] {
+            ret += 1;
+        }
+    }
+    ret
+}
