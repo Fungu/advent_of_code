796403e15ed9cbd7e8f7b976f4035297dacb2e4c
Fungu <christian.lundell@gmail.com>
2023-12-17 15:36:36 +0100

    Day05 refactoring

--- aoc_2023_csharp/day05.cs
+++ aoc_2023_csharp/day05.cs
@@ -10,8 +10,8 @@ public static class Day05
         var stopwatch = Stopwatch.StartNew();
 
         long[] ids = input[0].Split(": ")[1].Split(" ").Select(long.Parse).ToArray();
-        var levels = new List<List<(long, long, long)>>();
-        var level = new List<(long, long, long)>();
+        var levels = new List<List<(long destination, long source, long length)>>();
+        var level = new List<(long destination, long source, long length)>();
         for (int i = 3; i < input.Length;)
         {
             long destinationStart = long.Parse(input[i].Split(" ")[0]);
@@ -22,7 +22,6 @@ public static class Day05
             if (i >= input.Length || input[i] == "")
             {
                 i += 2;
-                level.Sort((x, y) => x.Item2.CompareTo(y.Item2));
                 levels.Add(level);
                 level = [];
             }
@@ -31,14 +30,14 @@ public static class Day05
         long part1 = ids.Min();
         var part1Results = new List<long>();
         for (int i = 0; i < ids.Length; i++)
-            part1Results.Add(Search(levels, 0, 0, ids[i], ids[i]));
+            part1Results.Add(Search(levels, 0, 0, (ids[i], ids[i] + 1)));
         part1 = part1Results.Min();
 
         var part2Results = new List<long>();
-        for (int i = 0; i <  ids.Length; i+=2) {
-            part2Results.Add(Search(levels, 0, 0, ids[i], ids[i] + ids[i + 1] - 1));
+        for (int i = 0; i <  ids.Length; i += 2) {
+            part2Results.Add(Search(levels, 0, 0, (ids[i], ids[i] + ids[i + 1])));
         }
-        long part2 = part2Results.Min() - 1;
+        long part2 = part2Results.Min();
 
         stopwatch.Stop();
         Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
@@ -46,54 +45,30 @@ public static class Day05
         Console.WriteLine($"Part 2: {part2}");
     }
 
-    private static long Search(List<List<(long, long, long)>> levels, int level, int index, long start, long end)
+    private static long Search(List<List<(long destination, long source, long length)>> levels, int level, int index, (long start, long end) range)
     {
         if (level >= levels.Count)
-            return start;
+            return range.start;
         else if (index >= levels[level].Count)
-            return Search(levels, level + 1, 0, start, end);
-        long destination = levels[level][index].Item1;
-        long source = levels[level][index].Item2;
-        long length = levels[level][index].Item3;
-        long offset = destination - source;
-        // --##--
-        // AA----
-        if (end < source)
-            return Search(levels, level + 1, 0, start, end);
-        // --##--
-        // ----AA
-        else if (start > source + length)
-            return Search(levels, level, index + 1, start, end);
-        // -####-
-        // --AA--
-        else if (start >= source && end <= source + length)
-            return Search(levels, level + 1, 0, start + offset, end + offset);
-        // --##--
-        // -ABBC-
-        else if (start < source && end > source + length)
-        {
-            long a = Search(levels, level, index, start, source - 1);
-            long b = Search(levels, level, index, source, source + length);
-            long c = Search(levels, level, index, source + length + 1, end);
-            return long.Min(a, long.Min(b, c));
-        }
-        // --##--
-        // -AB---
-        else if (start < source)
-        {
-            long a = Search(levels, level, index, start, source - 1);
-            long b = Search(levels, level, index, source, end);
-            return long.Min(a, b);
-        }
-        // --##--
-        // ---AB-
-        else if (end > source + length)
-        {
-            long a = Search(levels, level, index, start, source + length);
-            long b = Search(levels, level, index, source + length + 1, end);
-            return long.Min(a, b);
-        }
-        else
-            throw new Exception("Unhandled scenario");
+            return Search(levels, level + 1, 0, range);
+
+        long sourceStart = levels[level][index].source;
+        long sourceEnd = levels[level][index].length + sourceStart;
+        long offset = levels[level][index].destination - sourceStart;
+
+        List<long> result = [];
+
+        (long start, long end) before = (range.start, long.Min(range.end, sourceStart));
+        (long start, long end) overlap = (long.Max(range.start, sourceStart), long.Min(range.end, sourceEnd));
+        (long start, long end) after = (long.Max(range.start, sourceEnd), range.end);
+
+        if (before.end > before.start)
+            result.Add(Search(levels, level, index + 1, before));
+        if (overlap.end > overlap.start)
+            result.Add(Search(levels, level + 1, 0, (overlap.start + offset, overlap.end + offset)));
+        if (after.end > after.start)
+            result.Add(Search(levels, level, index + 1, after));
+        
+        return result.Min();
     }
 }
