56d9911bab3c78889b103f332dfb3d5b336ff2aa
Fungu <christian.lundell@gmail.com>
2023-12-18 12:41:38 +0100

    Improve performance

--- aoc_2023_csharp/day17.cs
+++ aoc_2023_csharp/day17.cs
@@ -13,9 +13,9 @@ public static class Day17
         for (int i = 0; i < input.Length; i++)
             grid[i] = input[i].Select(a => int.Parse(a.ToString())).ToArray();
 
-        int part1 = Search(grid, 0, 3);
+        int part1 = Search(grid, 1, 3);
         int part2 = Search(grid, 4, 10);
-
+        
         stopwatch.Stop();
         Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
         Console.WriteLine($"Part 1: {part1}");
@@ -24,45 +24,55 @@ public static class Day17
 
     private static int Search(int[][] grid, int minSteps, int maxSteps)
     {
-        Queue<(int x, int y, int dx, int dy, int steps)> openSet = [];
-        Dictionary<(int x, int y, int dx, int dy, int steps), int> closedSet = [];
-        openSet.Enqueue((0, 0, 1, 0, 0));
-        closedSet.Add((0, 0, 1, 0, 0), 0);
-        int ret = int.MaxValue;
+        int width = grid[0].Length;
+        int height = grid.Length;
+        Queue<(int x, int y, bool vertical)> openSet = [];
+        Dictionary<(int x, int y, bool vertical), int> closedSet = [];
+        openSet.Enqueue((0, 0, true));
+        openSet.Enqueue((0, 0, false));
+        closedSet.Add((0, 0, true), 0);
+        closedSet.Add((0, 0, false), 0);
         while (openSet.Count > 0)
         {
-            (int x, int y, int dx, int dy, int steps) = openSet.Dequeue();
-            int heat = closedSet[(x, y, dx, dy, steps)];
-            if (x == grid[0].Length - 1 && y == grid.Length - 1 && steps >= minSteps) ret = int.Min(ret, heat);
-
-            List<(int, int, bool)> directions = [];
-            if (steps < maxSteps) directions.Add((dx, dy, false));
-            if (steps >= minSteps) directions.Add((dy, dx, true));
-            if (steps >= minSteps) directions.Add((-dy, -dx, true));
-            foreach ((int ndx, int ndy, bool turn) in directions)
+            (int x, int y, bool vertical) = openSet.Dequeue();
+            int heat = closedSet[(x, y, vertical)];
+            (int x, int y)[] directions;
+            if (vertical)
+                directions = [(1, 0), (-1, 0)];
+            else
+                directions = [(0, 1), (0, -1)];
+            foreach ((int dx, int dy) in directions)
             {
-                (int nx, int ny) = (x + ndx, y + ndy);
-                if (nx >= 0 && ny >= 0 && nx < grid[0].Length && ny < grid.Length)
+                int subHeat = 0;
+                (int nx, int ny) = (x, y);
+                for (int step = 1; step <= maxSteps; step++)
                 {
-                    int nsteps = turn ? 1 : steps + 1;
-                    int nheat = heat + grid[ny][nx];
-                    if (closedSet.TryGetValue((nx, ny, ndx, ndy, nsteps), out int existingHeat))
+                    nx += dx;
+                    ny += dy;
+                    if (nx < 0 || ny < 0 || nx >= width || ny >= height) break;
+                    subHeat += grid[ny][nx];
+                    if (step >= minSteps)
                     {
-                        if (nheat < existingHeat)
+                        if (closedSet.TryGetValue((nx, ny, !vertical), out int existingHeat))
                         {
-                            if (!openSet.Contains((nx, ny, ndx, ndy, nsteps)))
-                                openSet.Enqueue((nx, ny, ndx, ndy, nsteps));
-                            closedSet[(nx, ny, ndx, ndy, nsteps)] = nheat;
+                            if (heat + subHeat < existingHeat)
+                            {
+                                if (!openSet.Contains((nx, ny, !vertical)))
+                                    openSet.Enqueue((nx, ny, !vertical));
+                                closedSet[(nx, ny, !vertical)] = heat + subHeat;
+                            }
                         }
-                    }
-                    else
-                    {
-                        openSet.Enqueue((nx, ny, ndx, ndy, nsteps));
-                        closedSet.Add((nx, ny, ndx, ndy, nsteps), nheat);
+                        else
+                        {
+                            openSet.Enqueue((nx, ny, !vertical));
+                            closedSet.Add((nx, ny, !vertical), heat + subHeat);
+                        }
                     }
                 }
             }
         }
-        return ret;
+        return int.Min(
+            closedSet[(width - 1, height - 1, true)], 
+            closedSet[(width - 1, height - 1, false)]);
     }
 }
