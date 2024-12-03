158eebffb072573a2be2bc709dab3e179ce60c1c
Fungu <christian.lundell@gmail.com>
2024-12-03 08:58:20 +0100

    minor cleanup

--- aoc_2024_python/day03.py
+++ aoc_2024_python/day03.py
@@ -2,19 +2,17 @@ import aoc
 import re
 
 def main(input: str):
-    r = re.compile(r'mul\((?P<a>\d+),(?P<b>\d+)\)')
-    part1 = sum([int(m.group("a")) * int(m.group("b")) for m in r.finditer(input)])
+    part1 = sum([int(m["a"]) * int(m["b"]) for m in re.finditer(r"mul\((?P<a>\d+),(?P<b>\d+)\)", input)])
 
     part2 = 0
     do = True
-    r = re.compile(r'(?P<do>do\(\))|(?P<dont>don\'t\(\))|(mul\((?P<a>\d+),(?P<b>\d+)\))');
-    for m in r.finditer(input):
-        if m.group("do"):
+    for m in re.finditer(r"(?P<do>do\(\))|(?P<dont>don't\(\))|(mul\((?P<a>\d+),(?P<b>\d+)\))", input):
+        if m["do"]:
             do = True
-        elif m.group("dont"):
+        elif m["dont"]:
             do = False
         elif do:
-            part2 += int(m.group("a")) * int(m.group("b"))
+            part2 += int(m["a"]) * int(m["b"])
 
     return part1, part2
 
