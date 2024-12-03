215a2cf5fb78cac015f655541a0c83a8e6c70d4e
Fungu <christian.lundell@gmail.com>
2024-12-03 08:58:20 +0100

    minor cleanup

--- aoc_2024_python/day01.py
+++ aoc_2024_python/day01.py
@@ -1,25 +1,6 @@
 import aoc
 
-def main(input_lines: list):
-    left = []
-    right = []
-    for v in input_lines:
-        left.append(int(v.split()[0]))
-        right.append(int(v.split()[1]))
-    left.sort()
-    right.sort()
-
-    part1 = 0
-    for i in range(len(input_lines)):
-        part1 += abs(left[i] - right[i])
-    
-    part2 = 0
-    for v in left:
-        part2 += v * right.count(v)
-
-    return part1, part2
-
-def main_golf(x: list):
+def main(x: list):
     a = sorted([int(v.split()[0]) for v in x])
     b = sorted([int(v.split()[1]) for v in x])
 
@@ -28,4 +9,4 @@ def main_golf(x: list):
 
     return part1, part2
 
-aoc.run_lines(main_golf, "day01.txt")
+aoc.run_lines(main, "day01.txt")
