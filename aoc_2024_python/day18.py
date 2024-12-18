5a3103966a5423c36d0dc9cd7f663bcacb4bd5f4
Fungu <christian.lundell@gmail.com>
2024-12-18 18:52:25 +0100

    fix typo

--- aoc_2024_python/day18.py
+++ aoc_2024_python/day18.py
@@ -10,20 +10,20 @@ def main(lines: list):
         a = line.split(",")
         blockers.append((int(a[0]), int(a[1])))
     
-    path = dfs(blockers[:1024], grid_size)
+    path = bfs(blockers[:1024], grid_size)
     part1 = len(path)
     
 
     for nr_of_bytes in range(1024, len(lines)):
         if blockers[nr_of_bytes - 1] in path:
-            path = dfs(blockers[:nr_of_bytes], grid_size)
+            path = bfs(blockers[:nr_of_bytes], grid_size)
         if not path:
             part2 = ",".join([str(a) for a in blockers[nr_of_bytes - 1]])
             break
 
     return part1, part2
 
-def dfs(blockers, grid_size):
+def bfs(blockers, grid_size):
     directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     grid_range = range(grid_size + 1)
     
